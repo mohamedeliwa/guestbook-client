c96bb9b80c40f245e6a4f3758d63e8c0a6e39294
mohamedeliwa <mohamed.av11@gmail.com>
2020-09-19 22:47:22 +0200

    form for new message created

--- components/Message.js
+++ components/Message.js
@@ -1,30 +1,50 @@
 import { Container, Media } from "react-bootstrap";
 import styles from "../styles/Message.module.css";
-
 const Message = () => {
   return (
-    <Container>
-      <Media>
+    <Container className="pt-5">
+      <Media className={styles.messageBox}>
         <Media.Body>
-          <h5>Media Heading</h5>
-          <p>
+          <h5 className={styles.username}>Jhone Deo</h5>
+          <p className={styles.messageContent}>
             Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
             scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
             in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
             nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
           </p>
-          <Media>
-            <Media.Body>
-              <h5>Media Heading</h5>
-              <p>
-                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-                scelerisque ante sollicitudin commodo. Cras purus odio,
-                vestibulum in vulputate at, tempus viverra turpis. Fusce
-                condimentum nunc ac nisi vulputate fringilla. Donec lacinia
-                congue felis in faucibus.
-              </p>
-            </Media.Body>
-          </Media>
+        </Media.Body>
+      </Media>
+      <Media className={styles.messageBox}>
+        <Media.Body>
+          <h5 className={styles.username}>Jhone Deo</h5>
+          <p className={styles.messageContent}>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
+            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </p>
+        </Media.Body>
+      </Media>
+      <Media className={styles.messageBox}>
+        <Media.Body>
+          <h5 className={styles.username}>Jhone Deo</h5>
+          <p className={styles.messageContent}>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
+            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </p>
+        </Media.Body>
+      </Media>
+      <Media className={styles.messageBox}>
+        <Media.Body>
+          <h5 className={styles.username}>Jhone Deo</h5>
+          <p className={styles.messageContent}>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
+            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </p>
         </Media.Body>
       </Media>
     </Container>
