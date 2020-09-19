cb02e26911fec1179a377aa013b80b71b0c8d4c5
mohamedeliwa <mohamed.av11@gmail.com>
2020-09-20 00:49:07 +0200

    user can logout

--- pages/board.js
+++ pages/board.js
@@ -1,11 +1,13 @@
 import { Container, Media } from "react-bootstrap";
 import Message from "../components/Message";
+import Navbar from "../components/Navbar";
 import NewMessageForm from "../components/NewMessageForm";
 import styles from "../styles/Board.module.css";
 
 const Board = () => {
   return (
     <Container className={styles.container}>
+      <Navbar />
       <Message />
       <NewMessageForm />
     </Container>
--- pages/message.js
+++ pages/message.js
@@ -2,10 +2,13 @@ import { Container, Media } from "react-bootstrap";
 import MessageThread from "../components/MessageThread";
 import NewReplyForm from "../components/NewReplyForm";
 import styles from "../styles/Board.module.css";
+import Navbar from "../components/Navbar";
+
 
 const Message = () => {
   return (
     <Container className={styles.container}>
+      <Navbar />
       <MessageThread />
       <NewReplyForm />
     </Container>
