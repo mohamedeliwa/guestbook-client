62567408c26ffd16ef6a4f19265de7f12a5b8006
mohamedeliwa <mohamed.av11@gmail.com>
2020-09-19 17:18:46 +0200

    some files added to the structure of the project

--- pages/board.js
+++ pages/board.js
@@ -1,26 +1,12 @@
 import { Container, Media } from "react-bootstrap";
+import Message from "../components/Message";
 
 const Board = () => {
   return (
     <Container>
-      <Media>
-        <img
-          width={64}
-          height={64}
-          className="mr-3"
-          src="/vercel.svg"
-          alt="Generic placeholder"
-        />
-        <Media.Body>
-          <h5>Media Heading</h5>
-          <p>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
-            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-          </p>
-        </Media.Body>
-      </Media>
+      <Message />
+      <a href="/board">Board</a>
+      <a href="/">Home</a>
     </Container>
   );
 };
