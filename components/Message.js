22c425bb007647cda032324c7af55b4650b81d13
mohamedeliwa <mohamed.av11@gmail.com>
2020-09-20 15:06:06 +0200

    user can create new messages

--- components/Message.js
+++ components/Message.js
@@ -1,56 +1,47 @@
 import Link from "next/link";
+import { useContext, useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import { Container, Media } from "react-bootstrap";
 import styles from "../styles/Message.module.css";
 
 const Message = () => {
-  return (
-    <Container className="pt-5">
-      <Link href="/message">
+  const router = useRouter();
+  const [state, setState] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        // end-point for reading profile info
+        const url = "http://localhost:5000/messages";
+        const response = await fetch(url, { credentials: "include" });
+        if (response.status === 200) {
+          setState(await response.json());
+        } else {
+          throw new Error("failed to load message, please try again later!");
+          alert("failed to load message, please try again later!");
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    })();
+  }, []);
+
+  const messages = state.reverse().map((message) => {
+    return (
+      <Link href={`/message?id=${message._id}`} key={message._id}>
         <Media className={styles.messageBox}>
           <Media.Body>
-            <h5 className={styles.username}>Jhone Deo</h5>
+            <h5 className={styles.username}>{message.owner.firstName + " " + message.owner.lastName}</h5>
             <p className={styles.messageContent}>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-              scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
-              in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-              nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+              {message.content}
             </p>
           </Media.Body>
         </Media>
       </Link>
-      <Media className={styles.messageBox}>
-        <Media.Body>
-          <h5 className={styles.username}>Jhone Deo</h5>
-          <p className={styles.messageContent}>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
-            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-          </p>
-        </Media.Body>
-      </Media>
-      <Media className={styles.messageBox}>
-        <Media.Body>
-          <h5 className={styles.username}>Jhone Deo</h5>
-          <p className={styles.messageContent}>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
-            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-          </p>
-        </Media.Body>
-      </Media>
-      <Media className={styles.messageBox}>
-        <Media.Body>
-          <h5 className={styles.username}>Jhone Deo</h5>
-          <p className={styles.messageContent}>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
-            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-          </p>
-        </Media.Body>
-      </Media>
+    );
+  });
+  return (
+    <Container className="pt-5">
+      {messages}
     </Container>
   );
 };
--- components/NewMessageForm.js
+++ components/NewMessageForm.js
@@ -1,15 +1,63 @@
+import { useContext, useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { AuthContext } from "../context/AuthContext";
 import styles from "../styles/NewMessageForm.module.css";
 import { Form, Button, Col, InputGroup, FormControl } from "react-bootstrap";
+
 const NewMessageForm = () => {
+  const router = useRouter();
+  const { user, isAuthenticated } = useContext(AuthContext);
+  const [state, setState] = useState({
+    content: "",
+  });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    switch (e.target.id) {
+      case "content":
+        setState({
+          ...state,
+          content: e.target.value,
+        });
+        break;
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = `http://localhost:5000/messages`;
+      const response = await fetch(url, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: state.content,
+        }),
+      });
+      if(response.status == 201){
+        router.reload();
+      }else{
+        throw new Error("creating message failed!")
+      }
+    } catch (error) {
+      console.log("error: ", error.message);
+    }
+  };
+
   return (
     <footer className={styles.footer}>
-      <Form className={styles.form}>
+      <Form className={styles.form} onSubmit={handleSubmit}>
         <Form.Row className="align-items-center">
           <Col xs="auto">
             <Form.Control
               className={styles.messageInputArea}
-              id="inlineFormInput"
+              id="content"
               placeholder="New message"
+              value={state.content}
+              onChange={handleChange}
             />
           </Col>
 
