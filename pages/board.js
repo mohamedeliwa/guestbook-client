e3c8a918c7486b97a2449cfa0e2d60f0f01a346b
mohamedeliwa <mohamed.av11@gmail.com>
2020-09-20 15:18:53 +0200

    redirecting user to home page if he is not authenticated

--- pages/board.js
+++ pages/board.js
@@ -1,10 +1,18 @@
-import { Container, Media } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { useContext, useEffect } from "react";
+import { useRouter } from "next/router";
 import Message from "../components/Message";
 import Navbar from "../components/Navbar";
 import NewMessageForm from "../components/NewMessageForm";
 import styles from "../styles/Board.module.css";
+import {AuthContext} from "../context/AuthContext";
 
 const Board = () => {
+  const router = useRouter();
+  const { isAuthenticated } = useContext(AuthContext);
+  useEffect(() => {
+    if (!isAuthenticated) router.push("/");
+  }, [isAuthenticated]);
   return (
     <Container className={styles.container}>
       <Navbar />
