eadb4458036288124bf9293dbbfd3e53c42bf438
mohamedeliwa <mohamed.av11@gmail.com>
2020-09-20 17:18:07 +0200

    user can reply to message and view all replies

--- pages/message.js
+++ pages/message.js
@@ -3,13 +3,21 @@ import MessageThread from "../components/MessageThread";
 import NewReplyForm from "../components/NewReplyForm";
 import styles from "../styles/Board.module.css";
 import Navbar from "../components/Navbar";
+import { useContext, useEffect } from "react";
+import { useRouter } from "next/router";
+import {AuthContext} from "../context/AuthContext";
 
 
 const Message = () => {
+  const router = useRouter();
+  const { isAuthenticated } = useContext(AuthContext);
+  useEffect(() => {
+    if (isAuthenticated === false) router.push("/");
+  }, [isAuthenticated]);
   return (
     <Container className={styles.container}>
       <Navbar />
-      <MessageThread />
+      {isAuthenticated ? <MessageThread messageID={router.query.id}/> : null}
       <NewReplyForm />
     </Container>
   );
